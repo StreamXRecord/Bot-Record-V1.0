fc800d0999f813d3778dfc822c50e5bb6769a73b
StreamXRecord <streamxrecord@gmail.com>
2024-10-12 23:19:35 +0700

    Add files via upload

---
+++ configs.py
@@ -0,0 +1,10 @@
+class Config:
+    # Required Variables
+    BOT_TOKEN = "7020754453:AAFKU8KUChqPDPKk6c8xAxP-VXgIrpKx_qg" #GET IT FROM @BOTFATHER
+    API_ID = "25957266" #GET IT FROM my.telegram.org
+    API_HASH = "e25868582faa883861842e3ea85cf60e" #GET IT FROM my.telegram.org
+    OWNER_ID = [5401929272] #GET IT FROM @getmyid_bot
+    MAX_RECORDINGS = 20 #Maximal processing url for recording 
+    SPLIT_DURATION = 1800 #Value duration Video will be splited as a seconds for example 1800 = 30 minutes and 3600 = 1 hour
+    # Optional Variables
+    CHANNEL_ID = -1001867625473 #GET IT FROM @showjsonbot
